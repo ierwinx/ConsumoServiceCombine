42b307da356c4816a0f9caeb8f29b1f228838b6e
Eяw!и Gontier <ierwinx@me.com>
2022-08-12 19:52:27 -0500

    Initial Commit

---
+++ ConsumoServ/ConsumoServApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumoServApp.swift
+//  ConsumoServ
+//
+//  Created by Erwin Luz León on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ConsumoServApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
